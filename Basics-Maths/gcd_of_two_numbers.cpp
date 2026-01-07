e1111a932b76f2ad5bd86606197eb83176538c43
Janhvi Bhawar <janhvibhawar28@gmail.com>
2026-01-07 20:57:49 +0530

    Implement GCD calculation for two numbers

---
+++ Basics-Maths/gcd_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    while (b != 0) {
+        int rem = a % b;
+        a = b;
+        b = rem;
+    }
+
+    cout << "GCD is: " << a << endl;
+    return 0;
+}
