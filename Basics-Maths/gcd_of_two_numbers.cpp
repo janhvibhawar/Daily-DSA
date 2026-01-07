4c1d6ab413e37d5cb4fd2cd496dece82f6e2db1d
Janhvi Bhawar <janhvibhawar28@gmail.com>
2026-01-07 21:13:33 +0530

    Find gcd of two numbers

--- Basics-Maths/gcd_of_two_numbers.cpp
+++ Basics-Maths/gcd_of_two_numbers.cpp
@@ -1,3 +1,11 @@
+/*
+Problem: GCD of Two Numbers
+Description: Find the greatest common divisor (GCD) of two positive integers.
+Approach: Use the Euclidean Algorithm (repeatedly replace a with b and b with a % b until b becomes 0).
+Time Complexity: O(log min(a, b))
+Space Complexity: O(1)
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,3 +22,5 @@ int main() {
     cout << "GCD is: " << a << endl;
     return 0;
 }
+
+#
